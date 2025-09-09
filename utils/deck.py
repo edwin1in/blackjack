3bc028a51f8f9a1f5a5bf95670cebb72fbd38d53
edwin1in <edwinzhanminglin@gmail.com>
2025-09-09 11:35:17 -0700

    deck class

---
+++ utils/deck.py
@@ -0,0 +1,25 @@
+from card import *
+from random import shuffle
+
+class Deck:
+
+    def __init__(self):
+        self.ranks = ["Ace", "2", "3", "4", "5", "6", 
+                 "7", "8", "9", "10", "Jack", "Queen", "King"]
+        
+        self.suits = ["Clubs", "Hearts", "Spades", "Diamonds"]
+
+        self.deck = []
+
+        for rank in self.ranks:
+            for suit in self.suits:
+                self.deck.append(Card(rank,suit))
+        
+        shuffle(self.deck)
+    
+    def __repr__(self):
+        return str([repr(x) for x in self.deck])
+
+    def size(self):
+        return len(self.deck)
+        
